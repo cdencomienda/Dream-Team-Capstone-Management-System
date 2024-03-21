8f0bd9a41dc4e02f1f4fb6e49add6e0d78c02e8d
Carlos Daniel Encomienda <71298875+cdencomienda@users.noreply.github.com>
2024-03-21 20:19:37 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+// var x = document.getElementById("login");
+// var y = document.getElementById("register");
+// var z = document.getElementById("btn");
+
+// function register(){
+//     x.style.left = "-438px";
+//     y.style.left = "212px";
+//     z.style.left = "110px";
+// }
