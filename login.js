b7b19e71d07053165c33cd97ff55d970af8adfc7
JohnEmerson22 <jmemerson41@gmail.com>
2024-03-25 00:36:43 +0800

    UPDATED feauture signup/login

--- login.js
+++ login.js
@@ -1,9 +1,39 @@
-// var x = document.getElementById("login");
-// var y = document.getElementById("register");
-// var z = document.getElementById("btn");
-
-// function register(){
-//     x.style.left = "-438px";
-//     y.style.left = "212px";
-//     z.style.left = "110px";
-// }
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        var z = document.getElementById("btn");
+        var w = document.getElementById("profile-upload");
+    
+        function auth(){
+            window.location.assign("HomePage.html")
+        }
+    
+        function register() {
+            
+            // w.style.left = "700px";
+            w.style.visibility = "visible";
+            x.style.left = "-488px";
+            y.style.left = "212px";
+            z.style.left = "110px";
+        }
+    
+        function login() {
+            w.style.visibility ="hidden";
+            w.style.left = "700px";
+            x.style.left = "212px";
+            y.style.left = "750px";
+            z.style.left = "0px";
+        }
+    
+        let profilePic = document.getElementById("profile-pic");
+        let inputFile = document.getElementById("input-file");
+    
+        inputFile.addEventListener("change", function() {
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e){
+                    profilePic.src = e.target.result;
+                }
+                reader.readAsDataURL(file);
+            }
+        });
