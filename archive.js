72f894c11728086d575ee43f8f187902398789ab
JohnEmerson22 <jmemerson41@gmail.com>
2024-03-26 22:07:34 +0800

    Finished archive table, and fixed UI

--- archive.js
+++ archive.js
@@ -1,54 +1,54 @@
- 
-const search = document.querySelector('.input-group input'),
-table_rows = document.querySelectorAll('tbody tr'),
-table_headings = document.querySelectorAll('thead th');
+const search = document.querySelector('.input-group input');
+const table_rows = document.querySelectorAll('tbody tr');
+const table_headings = document.querySelectorAll('thead th');
 
 // 1. Searching for specific data of HTML table
 search.addEventListener('input', searchTable);
 
 function searchTable() {
-table_rows.forEach((row, i) => {
-    let table_data = row.textContent.toLowerCase(),
-        search_data = search.value.toLowerCase();
+    table_rows.forEach((row, i) => {
+        let table_data = row.textContent.toLowerCase();
+        let search_data = search.value.toLowerCase();
 
-    row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
-    row.style.setProperty('--delay', i / 25 + 's');
-})
+        row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
+        row.style.setProperty('--delay', i / 25 + 's');
+    });
 
-document.querySelectorAll('tbody tr:not(.hide)').forEach((visible_row, i) => {
-    visible_row.style.backgroundColor = (i % 2 == 0) ? 'transparent' : '#0000000b';
-});
+    document.querySelectorAll('tbody tr:not(.hide)').forEach((visible_row, i) => {
+        visible_row.style.backgroundColor = (i % 2 == 0) ? 'transparent' : '#0000000b';
+    });
 }
 
 // 2. Sorting | Ordering data of HTML table
 
 table_headings.forEach((head, i) => {
-let sort_asc = true;
-head.onclick = () => {
-    table_headings.forEach(head => head.classList.remove('active'));
-    head.classList.add('active');
+    let sort_asc = true;
+    head.addEventListener('click', () => {
+        table_headings.forEach(head => head.classList.remove('active'));
+        head.classList.add('active');
 
-    document.querySelectorAll('td').forEach(td => td.classList.remove('active'));
-    table_rows.forEach(row => {
-        row.querySelectorAll('td')[i].classList.add('active');
-    })
+        document.querySelectorAll('td').forEach(td => td.classList.remove('active'));
+        table_rows.forEach(row => {
+            row.querySelectorAll('td')[i].classList.add('active');
+        });
 
-    head.classList.toggle('asc', sort_asc);
-    sort_asc = head.classList.contains('asc') ? false : true;
+        head.classList.toggle('asc', sort_asc);
+        sort_asc = head.classList.contains('asc') ? false : true;
 
-    sortTable(i, sort_asc);
-}
-})
+        sortTable(i, sort_asc);
+    });
+});
 
 
 function sortTable(column, sort_asc) {
-[...table_rows].sort((a, b) => {
-    let first_row = a.querySelectorAll('td')[column].textContent.toLowerCase(),
-        second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
+    const sortedRows = [...table_rows].sort((a, b) => {
+        const first_row = a.querySelectorAll('td')[column].textContent.toLowerCase();
+        const second_row = b.querySelectorAll('td')[column].textContent.toLowerCase();
 
-    return sort_asc ? (first_row < second_row ? 1 : -1) : (first_row < second_row ? -1 : 1);
-})
-    .map(sorted_row => document.querySelector('tbody').appendChild(sorted_row));
+        return sort_asc ? (first_row > second_row ? 1 : -1) : (first_row < second_row ? 1 : -1);
+    });
+
+    sortedRows.forEach(sorted_row => document.querySelector('tbody').appendChild(sorted_row));
 }
 
 // 3. Converting HTML table to PDF
@@ -58,20 +58,26 @@ const customers_table = document.querySelector('#customers_table');
 
 
 const toPDF = function (customers_table) {
-const html_code = `
-<!DOCTYPE html>
-<link rel="stylesheet" type="text/css" href="style.css">
-<main class="table" id="customers_table">${customers_table.innerHTML}</main>`;
+    const html_code = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body>
+            ${customers_table.innerHTML}
+        </body>
+    </html>`;
 
-const new_window = window.open();
- new_window.document.write(html_code);
+    const new_window = window.open();
+    new_window.document.write(html_code);
 
-setTimeout(() => {
-    new_window.print();
-    new_window.close();
-}, 400);
-}
+    setTimeout(() => {
+        new_window.print();
+        new_window.close();
+    }, 400);
+};
 
-pdf_btn.onclick = () => {
-toPDF(customers_table);
-}
+pdf_btn.addEventListener('click', () => {
+    toPDF(customers_table);
+});
