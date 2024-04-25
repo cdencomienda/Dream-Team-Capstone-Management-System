185834aac74ea93ddb65be05786382f3d6cff567
chan <baritchristiannn@gmail.com>
2024-04-26 01:08:35 +0800

    dont front end of submission frame with minor bugs

--- StudentCourse.php
+++ StudentCourse.php
@@ -92,8 +92,41 @@
                     </div>
                 </div>
                 <div class= "requirement-details">
-                    this requirement
-            </div>
+                    <div class="requirement-title" id="req_title"> <h3> Documentation requirement </h3> </div> <br>
+                    <div class="requirement-due" id="req_due"> <h4> Due : ??/??/???? </h4> </div><br>
+                    
+                    <div class="requirement-descriptionCont"> Requirement Description: 
+                        <div class="requirement-descBox" id="req_description"> 
+                            ?????
+                        </div>        
+                    </div>
+                    <div class="reqfile-version" id="reqfile_version"> <h4>Version: ?????</h4> 
+                    </div> <br>   
+                    <!-- idk kung san lalagay ung file attached id -->
+                    <div class="Attach-Files"> 
+                            <form id="file-upload" class="requirement-file">
+                                <div class="atchFiles">   
+                                    <h3>Attach Files   </h3> 
+                                    <!-- Replace img tag with the i tag -->
+                                    <!-- <i class="fa-solid fa-plus" id="attach-btn"></i> -->
+                                    <img src="course_assets/plus.png" alt="attached file" id="attach-btn"> 
+                                    <div id="upload-btn" class="upload-container"></div>
+                                    <label for="input-file"></label>
+                                    <input type="file" accept="pdf" id="input-file" name="profile_picture">
+                                </div>
+                            </form> 
+                        </div>   
+                        <div class="Attached-FileCont">
+                            <!-- attached files go here -->
+                        </div>        
+
+                    <div class="req-submitbtnCont"> 
+                        
+                        <button class="reqbtn" type="button">
+                            submit
+                        </button>
+                    </div>
+                </div>
             <div class="studentFilesR" id="studentFilesR">
                 <div class = "sFileContainer">
                 files repository
@@ -130,15 +163,63 @@
                 document.getElementById("submissionFrame").style.display = "none";
                 document.getElementById("studentFilesR").style.display = "flex";
             }
-
             // Get the element with the class "group_name"
             const groupDiv = document.getElementById('group_name');
-
             // Add an event listener to the element
             groupDiv.addEventListener('click', function() {
                 // Call the showDefaultBody function
                 showDefaultBody();
         });
+
+        // start
+        document.getElementById('input-file').addEventListener('change', function() {
+        var files = this.files; // Get the selected files
+
+        var fileList = document.createElement('ul'); // Create a list to hold file details
+        for (var i = 0; i < files.length; i++) {
+            // Check if the file is a PDF
+            if (files[i].type === 'application/pdf') {
+                var listItem = document.createElement('li'); // Create list item for each file
+                listItem.style.backgroundColor = '#F8EFE3'; // Set background color
+                listItem.style.borderRadius = '15px';
+                listItem.style.width = '245px';
+                listItem.style.height = '25px';
+
+                if (i > 0) {
+                    listItem.style.marginTop = '2px'; // Add margin to the top except for the first item
+                } else {
+                    listItem.style.marginTop = "3px";
+                }
+
+                // Create icon element
+                var fileIcon = document.createElement('i');
+                fileIcon.className = 'fa-regular fa-file-pdf'; // Set the class for the icon
+
+                // Create anchor element
+                var fileLink = document.createElement('a');
+                fileLink.textContent = files[i].name; // Set text content to file name
+                fileLink.href = URL.createObjectURL(files[i]); // Set href to the URL of the file
+                fileLink.download = files[i].name; // Set the download attribute to force download
+
+                // Append icon and anchor elements to list item
+                listItem.appendChild(fileIcon);
+                listItem.appendChild(fileLink);
+
+                // Append list item to list
+                fileList.appendChild(listItem);
+            } else {
+                alert("Only PDF files are allowed.");
+            }
+        }
+        var attachedFileCont = document.querySelector('.Attached-FileCont ul');
+        if (!attachedFileCont) {
+            attachedFileCont = document.createElement('ul');
+            attachedFileCont.style.padding = '1px'; // Add padding to the list
+            attachedFileCont.style.listStyleType = 'none'; // Remove default list style
+            document.querySelector('.Attached-FileCont').appendChild(attachedFileCont);
+        }
+        attachedFileCont.appendChild(fileList); // Append file list to Attached-FileCont
+    });
     </script>   
     </div>
 </body>
