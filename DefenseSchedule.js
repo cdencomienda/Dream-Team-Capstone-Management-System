e606fbbe2f80cb02af733a7c4096eda6aa1289eb
chan <baritchristiannn@gmail.com>
2024-05-09 02:59:10 +0800

    DefenseSchedule

---
+++ DefenseSchedule.js
@@ -0,0 +1,61 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("addScheduleBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function createDefenseContainer() {
+    // Gather input values
+    const schedTitle = document.getElementById('schedTitle').value;
+    const time = document.getElementById('time').value;
+    const date = document.getElementById('date').value;
+    const day = document.getElementById('day').value;
+
+    // Create new defense schedule container
+    const defenseContainer = document.createElement('div');
+    defenseContainer.classList.add('DefenseScheduleCont');
+    defenseContainer.innerHTML = `
+        <div class="SchedTitle">
+            <h2>${schedTitle}</h2>
+        </div>
+        <div class="SchedDetails">
+            day: ${day}<br>
+            time: ${time}<br>
+            date: ${date}<br>
+        </div>
+        <div class="DocumentStatus">
+            Status: Pending
+        </div>
+    `;
+
+    // Check if a new row needs to be created
+    let lastRow = document.querySelector('.ScheduleRow:last-child');
+    if (!lastRow || lastRow.children.length === 4) {
+        lastRow = document.createElement('div');
+        lastRow.classList.add('ScheduleRow');
+        document.getElementById('scheduleContainer').appendChild(lastRow);
+    }
+
+    // Append the new container to the last row
+    lastRow.appendChild(defenseContainer);
+}   
