30886bdaeca321277a1860e2f0875d0d6520b1d9
JohnEmerson22 <jmemerson41@gmail.com>
2024-07-02 17:53:34 +0800

    fixed some issues in admin

--- AdminHome.php
+++ AdminHome.php
@@ -6,7 +6,7 @@
     <title>Admin Main Menu</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
-        crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />  
         
     
     <style>
@@ -71,7 +71,7 @@
     </style>
 
     <link rel="stylesheet" href="AdminHomeStyle.css">
-    <?php include 'login.php'; ?>
+    <?php include 'login.php'; ?> 
     <?php include 'editProfile.php'; ?>
 
  
@@ -88,24 +88,23 @@
  
         <div class="container">
             <div class="action">
-                <div class="profile">
-                    <img src="menu_assets/users-icon.png" alt="profile-img">
+                <div class="profile" id="profilePic">
+                    <img src="menu_assets/prof.png" alt="profile-img">
                 </div>
-                <div class="menu" id ="menuBtn">
-                <h3><?php echo $_SESSION['fName'] . ' ' . $_SESSION['lname']; ?><br/>
+                <div class="menu" id="menuBtn">
+                    <h3><?php echo $_SESSION['fName'] . ' ' . $_SESSION['lname']; ?><br/>
                         <span><?php echo $_SESSION['user_email']; ?></span>
                     </h3>
                     <button type="button" class="editprofileBtn" id="editProfileBtn">Edit Profile</button>
                     <button type="button" class="logoutBtn" onclick="logOUT()">Logout</button>
                 </div>
             </div>
-            <!-- editprofile -->
+            <!-- editprofile --> 
             <div id="editProfileOverlay" class="editoverlay">
                 <div class="dropdown-profile">
                     <div>
-                        <button class = "close" onclick= "closeEditform()">  <i class="fa-regular fa-circle-xmark"></i> </button>
+                        <button class="close" onclick="closeEditform()"><i class="fa-regular fa-circle-xmark"></i></button>
                     </div>
-                    
                     <form id="editProfileForm" action="editProfile.php" method="POST">
                         <div class="profile">
                             <img src="menu_assets/prof.png" alt="profile-img">
@@ -113,25 +112,24 @@
                         <h5edit><?php echo $_SESSION['fName'] . ' ' . $_SESSION['lname']; ?><br/>
                         <span><?php echo $_SESSION['user_email']; ?></span>
                         </h5edit>
-                        <h3> <input type="text" id="profileemailID" class="inputEmail" name="userEmail" placeholder="Input your Email"> </h3>
-                        <h3> <input type="text" id="profileFnameID" class="inputname" name="newFname" placeholder="Input new First Name"> </h3>
-                        <h3> <input type="text" id="profileLnameID" class="inputname" name="newLname" placeholder="Input new Last Name"> </h3>
-                        <h3> <input type="text" id="profilepasswordID" class="inputPassword" name="newPassword" placeholder="Input new Password"> </h3>
-                        <button type="submit" class="saveEditbtn"> Save Changes </button>
-                       
+                        <h3><input type="text" id="profileemailID" class="inputEmail" name="userEmail" placeholder="Input your Email"></h3>
+                        <h3><input type="text" id="profileFnameID" class="inputname" name="newFname" placeholder="Input new First Name"></h3>
+                        <h3><input type="text" id="profileLnameID" class="inputname" name="newLname" placeholder="Input new Last Name"></h3>
+                        <h3><input type="text" id="profilepasswordID" class="inputPassword" name="newPassword" placeholder="Input new Password"></h3>
+                        <button type="submit" class="saveEditbtn">Save Changes</button>
                     </form>
-                   
                     <?php if(isset($_SESSION['error_message'])) { ?>
-                <div id="error-message" class="show">
- 
-                    <?php echo $_SESSION['error_message']; ?>
-                    <button onclick="clearErrorMessage()">OK</button>
+                    <div id="error-message" class="show">
+                        <?php echo $_SESSION['error_message']; ?>
+                        <button onclick="clearErrorMessage()">OK</button>
+                    </div>
+                    <?php unset($_SESSION['error_message']); } ?>
                 </div>
-            <?php
-                unset($_SESSION['error_message']); // Clear the error message after displaying it
-            } ?>
+            </div>
+        </div>
+
  
- <script>
+    <script>
         window.onload = function() {
             var urlParams = new URLSearchParams(window.location.search);
  
@@ -183,3 +181,4 @@
 <script src="adminHome.js"></script>   
 </body>
 </html>
+
